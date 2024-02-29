6c933f3132b03c4184b3ae49d33fe1d4cd8879f8
Ben Wilson <ben.wilson@smartbear.com>
2024-02-29 16:27:34 +0000

    test: :white_check_mark: add HandledJsError scenario

---
+++ test/features/fixtures/keplertestapp/src/scenarios/HandledJsError.tsx
@@ -0,0 +1,17 @@
+import Bugsnag from '@bugsnag/kepler'
+import React, { useEffect } from 'react'
+import { Text, View } from "react-native"
+
+export const config = {}
+
+export const App = () => {
+    useEffect(() => {
+        Bugsnag.notify(new Error('HandledJSError'))
+    }, [])
+
+    return (
+        <View>
+            <Text>HandledJsError</Text>
+        </View>
+    )
+}
---
+++ test/features/fixtures/keplertestapp/src/scenarios/index.ts
@@ -0,0 +1,1 @@
+export * as HandledJsError from './HandledJSError'
