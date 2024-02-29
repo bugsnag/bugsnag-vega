4ba18e41cb72282b1d35c90d829ae8b7eb8ac1ca
Daria Bialobrzeska <daria.bialobrzeska@smartbear.com>
2024-02-29 19:28:51 +0100

    Added polling for command

--- test/features/fixtures/keplertestapp/src/getCommand.ts
+++ test/features/fixtures/keplertestapp/src/getCommand.ts
@@ -1,12 +1,48 @@
 import type { Command } from "../types"
 
-export async function getCommand (): Promise<Command> {
-    // poll the server for the next command to run
+const DEFAULT_RETRY_COUNT = 20
+const INTERVAL = 500
 
-    return {
+function getErrorMessage(error: unknown) {
+    if (error instanceof Error) return error.message
+    return String(error)
+  }
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+export async function getCommand (mazeAddress = 'bs-local.com:9339', allowedRetries = DEFAULT_RETRY_COUNT): Promise<Command> {
+    // poll the server for the next command to run
+    const mazeUrl = `http://${mazeAddress}/command`
+
+    console.error('getCommand entered!')
+
+   return {
         action: 'run-scenario',
         scenario_name: 'TestScenario1',
         api_key: '1234',
         endpoint: '/test'
     }
+
+    let retries = 0
+    while (retries++ < allowedRetries) {
+        try {
+            console.error('Fetching command from maze')
+            const response = await fetch(mazeUrl)
+            const command: Command = await response.json()
+            if (response.ok) {
+                if (command?.action !== 'noop') {
+                    return command!
+                }
+            } else {
+                console.error('Error while receiving command')
+            }
+        } catch (err) {
+            console.error(`[BugsnagPerformance] Error fetching command from maze runner: ${getErrorMessage(err)}`)
+        }
+      await delay(INTERVAL)
+    }
+
+      throw new Error('Retry limit exceeded, giving up...')
 }
