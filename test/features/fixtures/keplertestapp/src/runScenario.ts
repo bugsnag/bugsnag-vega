f88ba7aa7d778135e690e39600ed84ae05087d46
Ben Wilson <ben.wilson@smartbear.com>
2024-02-29 16:29:30 +0000

    test: :white_check_mark: implement scenario runner using AppRegistry and Bugsnag client

--- test/features/fixtures/keplertestapp/src/runScenario.ts
+++ test/features/fixtures/keplertestapp/src/runScenario.ts
@@ -1,3 +1,30 @@
-export const runScenario = (rootTag: number, scenarioName: string, apiKey: string, endpoint: string) => {
+import Busgnag from '@bugsnag/kepler'
+import { AppRegistry } from 'react-native'
+import * as Scenarios from './scenarios'
 
-}
+type ScenarioName = keyof typeof Scenarios
+
+// @ts-expect-error
+const isTurboModuleEnabled = () => global.__turboModuleProxy != null
+
+export async function runScenario (rootTag: number, scenarioName: string, apiKey: string, endpoint: string) {
+    console.error(`[Busgnag] Launching scenario: ${scenarioName}`)
+    const scenario = Scenarios[scenarioName as ScenarioName]
+
+    const config = {
+        apiKey,
+        endpoints: { notify: endpoint, sessions: endpoint },
+        ...scenario.config
+    }
+  
+    Busgnag.start(config)
+  
+    const appParams = { rootTag }
+    // if (isTurboModuleEnabled()) {
+    //     appParams.fabric = true
+    //     appParams.initialProps = { concurrentRoot: true }
+    // }
+
+    AppRegistry.registerComponent(scenarioName, () => scenario.App)
+    AppRegistry.runApplication(scenarioName, appParams)
+  }
