81860a92ce646f7ded908e4b60a8176d601a6768
Daria Bialobrzeska <daria.bialobrzeska@smartbear.com>
2024-03-04 15:26:15 +0100

    Print errors without using toString func

--- test/features/fixtures/keplertestapp/src/utils/storage.ts
+++ test/features/fixtures/keplertestapp/src/utils/storage.ts
@@ -6,7 +6,7 @@ export async function setLastCommandUUID(uuid: string) {
     try {
         await AsyncStorage.setItem(LAST_COMMAND_KEY, uuid)
     } catch (error) {
-        console.log(`[Bugsnag] Could not save last command uuid in storage: ${error}`)
+        console.log(`[Bugsnag] Could not save last command uuid in storage: `, error)
         throw error
     }
 }
@@ -16,7 +16,7 @@ export async function getLastCommandUUID() {
         const value = await AsyncStorage.getItem(LAST_COMMAND_KEY)
         return value
     } catch (error) {
-        console.log(`[Bugsnag] Could not read last command uuid from storage: ${error}`)
+        console.log(`[Bugsnag] Could not read last command uuid from storage: `, error)
         throw error
     }
 }
@@ -25,7 +25,7 @@ export async function clearLastCommandUUID() {
     try {
         await AsyncStorage.removeItem(LAST_COMMAND_KEY)
     } catch (error) {
-        console.log(`[Bugsnag] Could not remove last command uuid from storage: ${error}`)
+        console.log(`[Bugsnag] Could not remove last command uuid from storage: `, error)
         throw error
     }
 }
