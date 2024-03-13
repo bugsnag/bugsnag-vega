fd72fdc2646bf4bc42645f2fca42e193436523ae
Yousif Ahmed <yousif@bugsnag.com>
2024-03-13 13:37:39 +0000

    test: add e2e test for onSession callbacks

---
+++ test/features/fixtures/keplertestapp/src/scenarios/OnSessionCallbackScenario.tsx
@@ -0,0 +1,35 @@
+import Bugsnag, { type KeplerConfig } from '@bugsnag/kepler'
+import React, { useEffect } from 'react'
+import { Text, View } from "react-native"
+import { getStyles } from '../utils/defaultStyle'
+
+const config: Partial<KeplerConfig> = {
+    autoTrackSessions: false,
+    onSession: (session) => {
+        if (session.app) session.app.releaseStage = 'test'
+    }
+}
+
+const App = () => {
+    const styles = getStyles()
+
+    useEffect(() => {
+        Bugsnag.addOnSession((session) => {
+            session.setUser('123', 'test@bugsnag.com', 'Test User')
+        })
+
+        Bugsnag.startSession()
+    },[])
+
+    return (
+        <View style={styles.background}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerText}>OnSessionCallbackScenario</Text>
+            </View>
+        </View>
+    )
+}
+
+export default { App, config }
+
+
