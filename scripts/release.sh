d9da517b1c4ae0bd5770f59f2a8de9ec451b107b
Yousif Ahmed <yousif@bugsnag.com>
2025-09-16 14:44:30 +0100

    chore: add release script

---
+++ scripts/release.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+error_missing_field () {
+  echo "Missing required env var: $1"
+  exit 1
+}
+
+# Ensure all required variables are set before doing any work
+if [[ -z ${GITHUB_USER:-} ]]; then error_missing_field "GITHUB_USER"; fi
+if [[ -z ${GITHUB_ACCESS_TOKEN:-} ]]; then error_missing_field "GITHUB_ACCESS_TOKEN"; fi
+if [[ -z ${RELEASE_BRANCH:-} ]]; then error_missing_field "RELEASE_BRANCH"; fi
+if [[ -z ${VERSION:-} ]]; then error_missing_field "VERSION"; fi
+if [[ -z ${DIST_TAG:-} ]]; then error_missing_field "DIST_TAG"; fi
+
+git clone --single-branch --recursive \
+  --branch "$RELEASE_BRANCH" \
+  https://"$GITHUB_USER":"$GITHUB_ACCESS_TOKEN"@github.com/bugsnag/bugsnag-js.git
+
+cd /app/bugsnag-kepler
+
+# "ci" rather than "install" ensures the process doesn't make the work tree dirty by modifying lockfiles
+npm ci
+
+# increment package version numbers
+if [ -z "${RETRY_PUBLISH:-}" ]; then
+  # ensure we don't use legacy peer deps when bumping versions (https://github.com/nrwl/nx/issues/22066)
+  npm_config_legacy_peer_deps=false npx lerna version "$VERSION"  --no-push --no-private
+fi
+
+# push version bump commit and tags
+git push --follow-tags
+
+# publish
+if [ -z "${RETRY_PUBLISH:-}" ]; then
+  case $VERSION in
+    "prerelease" | "prepatch" | "preminor" | "premajor")
+      npx lerna publish from-git --dist-tag next
+      ;;
+
+    *)
+      npx lerna publish from-git
+      ;;
+  esac
+else
+  case $VERSION in
+    "prerelease" | "prepatch" | "preminor" | "premajor")
+      npx lerna publish from-package --dist-tag next
+      ;;
+
+    *)
+      npx lerna publish from-package
+      ;;
+  esac
+fi
